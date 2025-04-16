e4794ba4fa6f009d884f01c3f2d693b5e58cda72
ScriptSorcerer20 <jerome.bachmann3@gmail.com>
2025-04-16 14:48:24 +0200

    another start commit

--- gambling.js
+++ gambling.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const path = require("path");
 const port = 3000;
 
 app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (request, respond) => {
-    respond.send("Test")
+    respond.sendFile(path.join(__dirname, 'structure.png'));
 });
 
 app.listen(port, () => {
